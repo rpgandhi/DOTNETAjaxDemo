b405c0da219debc6da7499309b5133e87ba756b7
Kimberly Bordon and Rakhee Gandhi <kimberly+rakhee@>
2018-01-17 13:27:09 -0800

    Follow AJAX lessons.

---
+++ AjaxDemo/Models/AjaxDemoContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxDemo.Models
+{
+    public class AjaxDemoContext : DbContext
+    {
+        public DbSet<Destination> Destinations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;Port=8889;database=ajaxdemo;uid=root;pwd=root;");
+    }
+}
